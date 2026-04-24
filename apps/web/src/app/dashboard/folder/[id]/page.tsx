ebf93e84fd4e2a652790a186d0b6707d3d3fce6c
ravinder9280 <ravinder92809@gmail.com>
2026-04-24 15:35:01 +0530

    feat: add image view, search, imageview 3

--- apps/web/src/app/dashboard/folder/[id]/page.tsx
+++ apps/web/src/app/dashboard/folder/[id]/page.tsx
@@ -1,9 +1,8 @@
 import FolderClient from "./_components/FolderClient";
 
-
-const FolderPage = async ({ params }: { params: { id?: string } }) => {
-  const {id} = await params;
-  return <FolderClient id={id||null} />;
+const FolderPage = async ({ params }: { params: Promise<{ id?: string }> }) => {
+  const { id } = await params;
+  return <FolderClient id={id || null} />;
 };
 
 export default FolderPage;
